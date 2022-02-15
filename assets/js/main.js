a261b96ea4b35e151a7fb336a11381c754b6db3d
bedimcode <61248995+bedimcode@users.noreply.github.com>
2022-02-15 08:17:51 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/*=============== LOADER ===============*/
+
+
+/*=============== SHOW MENU ===============*/
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== MIXITUP FILTER PRODUCTS ===============*/
+
+
+/* Default filter products */ 
+
+
+/* Link active products */ 
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
